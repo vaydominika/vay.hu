61f8b39fb71a9a7d93d76f677ea7a2e001f70ac4
vaydominika <vaydominika@gmail.com>
2025-08-16 21:27:38 +0200

    style: changed hero's background svg and added title to it

--- app/pages/home.tsx
+++ app/pages/home.tsx
@@ -10,11 +10,14 @@ export default function HomePage() {
       
       {/* Typewriter Text Overlay */}
       <div className="absolute inset-0 flex items-center">
-        <div className="ml-[190px] px-16 transform translate-y-24">
+        <div className="ml-[280px] transform -translate-y-18">
+        <h1 className="text-[200px] bg-gradient-to-tr from-[#4A55A1] via-third to-[#4A55A1] bg-clip-text text-transparent" style={{ fontFamily: 'Des Montilles' }}>
+            Vay Dominika
+        </h1>
           <Typewriter 
             text="A digital explorer turning ideas into interactive web experiences with a touch of creativity."
             speed={40}
-            className="text-2xl font-medium text-third leading-relaxed max-w-4xl drop-shadow-lg"
+            className="text-2xl font-medium text-third leading-relaxed max-w-4xl drop-shadow-lg opacity-75"
           />
         </div>
       </div>
