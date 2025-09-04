ae4a5adf44121c513db4551129d2b446f285825b
vaydominika <vaydominika@gmail.com>
2025-09-04 21:43:02 +0200

    feat: export Carousel and CardWithDescription components in UI index for improved accessibility

--- app/components/ui/index.ts
+++ app/components/ui/index.ts
@@ -1,3 +1,5 @@
 export { default as Button } from './Button';
 export { default as Card } from './Card';
 export { default as Typewriter } from './Typewriter';
+export { default as Carousel } from './Carousel';
+export { default as CardWithDescription } from './CardWithDescription';
